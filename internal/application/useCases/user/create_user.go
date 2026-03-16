b1e3c175691246ac298ee2282e959116a2cb270a
gfloriano11 <gflorianodev@gmail.com>
2026-03-16 20:14:28 -0300

    feat: implements create user method

--- internal/application/useCases/user/create_user.go
+++ internal/application/useCases/user/create_user.go
@@ -4,6 +4,7 @@ import (
 	inputs "chat-service/internal/application/Inputs"
 	"chat-service/internal/domain/user"
 	"chat-service/internal/infrastructure/database/repository"
+	"errors"
 )
 
 type CreateUserUseCase struct {
@@ -17,5 +18,18 @@ func NewCreateUserUseCase(repository repository.UserRepository) *CreateUserUseCa
 }
 
 func (useCase CreateUserUseCase) CreateUser(createUserInput inputs.CreateUserInput) (user.User, error) {
-	
+
+	if createUserInput.Email == "" || createUserInput.Fullname == "" || createUserInput.Username == "" || createUserInput.Password == "" {
+		return user.User{}, errors.New("Any of the fields can be empty")
+	}
+
+	user := &user.User{
+		Email: createUserInput.Email,
+		Username: createUserInput.Username,
+		Fullname: createUserInput.Fullname,
+	}
+
+	useCase.Repository.Save(*user)
+
+	return *user, nil
 }
