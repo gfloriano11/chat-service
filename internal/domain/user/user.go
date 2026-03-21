25a63ef198a7c73ae82ce2c5c48378dbaa2aa2d7
gfloriano11 <gflorianodev@gmail.com>
2026-03-20 23:22:05 -0300

    feat: add password column in user table

--- internal/domain/user/user.go
+++ internal/domain/user/user.go
@@ -6,10 +6,11 @@ type User struct {
 	Id        int
 	Fullname  string
 	Email     string
+	Password 	string
 	Username  string
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	UpdatedBy int
+	UpdatedBy *int
 	IsDeleted bool
 	DeletedAt *time.Time
 	DeletedBy *int
--- internal/infrastructure/database/entity/user.go
+++ internal/infrastructure/database/entity/user.go
@@ -6,10 +6,11 @@ type User struct {
 	Id 				int					`gorm:"primaryKey"`
 	Fullname 	string
 	Email 		string			`gorm:"type:varchar(255)"`
+	Password 	string			`gorm:"type:varchar(255)"`
 	Username 	string 		 	`gorm:"type:varchar(255)"`
 	CreatedAt time.Time  	`gorm:"type:timestamp"`
 	UpdatedAt time.Time  	`gorm:"type:timestamp"`
-	UpdatedBy int
+	UpdatedBy *int
 	IsDeleted bool
 	DeletedAt *time.Time 	`gorm:"type:timestamp"`
 	DeletedBy *int
